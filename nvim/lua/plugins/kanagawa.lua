efe1c3aad1f7f8913864de0637b14ef12aba357e
jpfogato <jpfogato@gmail.com>
2025-10-13 20:46:22 +0200

    added statusline

--- nvim/lua/plugins/kanagawa.lua
+++ nvim/lua/plugins/kanagawa.lua
@@ -1,3 +1,14 @@
 return {
-	"rebelot/kanagawa.nvim", opts = {}
+	"rebelot/kanagawa.nvim", 
+	config=function()
+		require('kanagawa').setup({
+			compile=true,
+			transparent=false,
+		});
+		vim.cmd("colorscheme kanagawa");
+	end,
+	build=function()
+		vim.cmd("KanagawaCompile");
+	end,
+
 }
