2b389c5dba5558822c19094844266579eb1f7068
jpfogato <jpfogato@gmail.com>
2025-10-16 21:11:15 +0200

    additional settings for selection

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -23,6 +23,15 @@ return {
       indent = {
         enable = true,
       },
-    })
+
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<Enter>",
+          node_incremental = "<Enter>",
+          scope_incremental = false, 
+          node_decremental = "<Backspace>",
+        },
+      }})
   end,
 }
