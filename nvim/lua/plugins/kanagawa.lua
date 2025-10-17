3246a868b218c67199c951626f99fa0202c16d5d
jpfogato <jpfogato@gmail.com>
2025-10-17 20:30:40 +0200

    added textobjects for tree-sitter

--- nvim/lua/plugins/kanagawa.lua
+++ nvim/lua/plugins/kanagawa.lua
@@ -4,6 +4,18 @@ return {
 		require('kanagawa').setup({
 			compile=true,
 			transparent=false,
+			overrides = function(colors)
+				return {
+					["@markup.link.url.markdown_inline"] = {link = "Special"}, -- (url)
+					["@markup.link.label.markdown_inline"] = {link = "WarningMsg"},-- [label] 
+					["@markup.italic.markdown_inline"] = {link = "Exception"},-- *italic* 
+					["@markup.raw.markdown_inline"] = {link = "String"}, -- `code`
+					["@markup.list.markdown"] = {link = "Function"}, -- + list
+					["@markup.quote.markdown"] = {link = "Error"}, -- >blockcode
+					["@markup.link.checked.markdown"] = {link = "WarningMsg"}, -- - [ X ] 
+				}
+			end,
+			theme = "lotus",
 		});
 		vim.cmd("colorscheme kanagawa");
 	end,
