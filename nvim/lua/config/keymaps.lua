95dfef2857f8383a0c4980995a688f381f622b83
jpfogato <jpfogato@gmail.com>
2025-10-23 18:40:57 +0200

    changed tabs plugin

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -77,13 +77,8 @@ keymap("t", "<C-l>", [[<C-\><C-n><C-w>l]], opts)
 
 -- barbar binds
 -- Move to previous/next
-keymap("n", "<Leader><Tab>", "<Cmd>BufferNext<CR>", { noremap = true, silent = true, desc = "next buffer tab" })
-keymap(
-	"n",
-	"<Leader><Backspace>",
-	"<Cmd>BufferPrevious<CR>",
-	{ noremap = true, silent = true, desc = "prev buffer tab" }
-)
+keymap("n", "<Leader><Tab>", "<Cmd>bnext<CR>", { noremap = true, silent = true, desc = "next buffer tab" })
+keymap("n", "<Leader><Backspace>", "<Cmd>bprevious<CR>", { noremap = true, silent = true, desc = "prev buffer tab" })
 
 -- Goto buffer in position...
 keymap("n", "<A-1>", "<Cmd>BufferGoto 1<CR>", opts)
@@ -97,51 +92,6 @@ keymap("n", "<A-8>", "<Cmd>BufferGoto 8<CR>", opts)
 keymap("n", "<A-9>", "<Cmd>BufferGoto 9<CR>", opts)
 keymap("n", "<A-0>", "<Cmd>BufferLast<CR>", opts)
 
--- Pin/unpin buffer
-keymap("n", "<A-p>", "<Cmd>BufferPin<CR>", opts)
-
--- Goto pinned/unpinned buffer
---                 :BufferGotoPinned
---                 :BufferGotoUnpinned
-
--- Close buffer
-keymap("n", "<A-c>", "<Cmd>BufferClose<CR>", opts)
-
--- Wipeout buffer
---                 :BufferWipeout
-
--- Close commands
---                 :BufferCloseAllButCurrent
---                 :BufferCloseAllButPinned
---                 :BufferCloseAllButCurrentOrPinned
---                 :BufferCloseBuffersLeft
---                 :BufferCloseBuffersRight
-
--- Magic buffer-picking mode
--- keymap("n", "<C-p>", "<Cmd>BufferPick<CR>", opts)
--- keymap("n", "<C-s-p>", "<Cmd>BufferPickDelete<CR>", opts)
-
--- Sort automatically by...
-keymap(
-	"n",
-	"<Space>Tb",
-	"<Cmd>BufferOrderByBufferNumber<CR>",
-	{ noremap = true, silent = true, desc = "order [b]y number" }
-)
-keymap("n", "<Space>To", "<Cmd>BufferOrderByName<CR>", { noremap = true, silent = true, desc = "[o]rder by name" })
-keymap("n", "<Space>Td", "<Cmd>BufferOrderByDirectory<CR>", { noremap = true, silent = true, desc = "order by [d]ir" })
-keymap(
-	"n",
-	"<Space>Tl",
-	"<Cmd>BufferOrderByLanguage<CR>",
-	{ noremap = true, silent = true, desc = "order by [l]anguage" }
-)
-keymap(
-	"n",
-	"<Space>Tw",
-	"<Cmd>BufferOrderByWindowNumber<CR>",
-	{ desc = "order by [w]indow number", noremap = true, silent = true }
-)
-
+-- window resizing
 keymap("n", "<A->>", "<Cmd>:vertical resize +2<CR>", { noremap = true, silent = true })
 keymap("n", "<A-<>", "<Cmd>:vertical resize -2<CR>", { noremap = true, silent = true })
